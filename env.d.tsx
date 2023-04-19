0daeae8e4d347eed3ce8af094728bff92fef1860
Alexey Rodionov <arodionov.dev@gmail.com>
2023-04-20 01:56:43 +0600

    Add dotenv and config

---
+++ env.d.tsx
@@ -0,0 +1,5 @@
+declare module '@env' {
+  export const API_URL: string;
+  export const API_TOKEN: string;
+  export const GITHUB_API_VERSION: string;
+}
---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+import {API_URL, GITHUB_API_VERSION, API_TOKEN} from '@env';
+
+export default Object.freeze({
+  apiUrl: API_URL,
+  apiToken: API_TOKEN,
+  githubApiVersion: GITHUB_API_VERSION,
+});
